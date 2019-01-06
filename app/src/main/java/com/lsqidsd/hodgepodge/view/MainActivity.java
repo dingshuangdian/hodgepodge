f738ef653373e30b3b7f657fb18cc5e5edf69834
dingshuangdian <286502698@qq.com>
2019-01-06 20:04:30 +0800

    Initial commit

---
+++ app/src/main/java/com/lsqidsd/hodgepodge/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lsqidsd.hodgepodge.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lsqidsd.hodgepodge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
